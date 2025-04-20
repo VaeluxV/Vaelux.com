10de03113ab28726d0fd1ec497beba00e59f9a9e
Vaelux <inquiries.vaelux@outlook.com>
2025-04-20 20:26:29 +0200

    Made initial art files and test map
    Made initial art files and test map. These do not fall under the main project license and thus I hold the exclusive copyrights for now. I am planning to add a proper licensing to these such as a creative commons license or similar. Note that none of this art is final yet, it might change in the future!

---
+++ projects/assets/Tiled-Project/ground_tiles_v007.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ground_tiles_v007" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../source_files/ground_tiles_v007.png" width="256" height="256"/>
+ <tile id="0" type="grass_detailed"/>
+ <tile id="1" type="grass_general"/>
+ <tile id="2" type="grass_flat"/>
+ <tile id="9" type="grass_ballast_south"/>
+ <tile id="10" type="grass_ballast_west"/>
+ <tile id="11" type="grass_ballast_all_sides"/>
+ <tile id="12" type="grass_ballast_three_side_east"/>
+ <tile id="13" type="grass_ballast_three_side_west"/>
+ <tile id="14" type="grass_ballast_two_side_north_west"/>
+ <tile id="15" type="grass_ballast_two_side_north_east"/>
+ <tile id="25" type="grass_ballast_east"/>
+ <tile id="26" type="grass_ballast_north"/>
+ <tile id="27" type="ballast"/>
+ <tile id="28" type="grass_ballast_three_side_north"/>
+ <tile id="29" type="grass_ballast_three_side_south"/>
+ <tile id="30" type="grass_ballast_two_side_south_west"/>
+ <tile id="31" type="grass_ballast_two_side_south_east"/>
+ <tile id="44" type="rail_ballast_crossing"/>
+ <tile id="45" type="rail_ballast_curve_south_west"/>
+ <tile id="46" type="rail_ballast_curve_north_west"/>
+ <tile id="47" type="rail_ballast_horizontal"/>
+ <tile id="61" type="rail_ballast_curve_south_east"/>
+ <tile id="62" type="rail_ballast_curve_north_east"/>
+ <tile id="63" type="rail_ballast_vertical"/>
+</tileset>
